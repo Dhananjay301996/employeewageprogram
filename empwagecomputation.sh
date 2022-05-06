387c0c35e545eb140d373927b5561e062c52afc8
Dhananjay301996 <gadhavedhananjay123@gmail.com>
2022-05-06 19:32:28 +0530

    emp wage

---
+++ empwagecomputation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+echo "welcome"
+echo "welcome"
+
+isparttime=1;
+isfulltime=2;
+emprateperhr=20;
+workingdays=20;
+totalsalary=0;
+
+for (( day=1; day<=$workingdays; day++ ))
+do
+	empcheck=$(( RANDOM%3 ))
+	case $empcheck in
+		$isfulltime)
+			emphrs=8;;
+		$isparttime)
+			emphrs=4;;
+		*)
+			emphrs=0;;
+	esac
+
+salary=$(($emphrs * $emprateperhr));
+totalsalary=$(( $totalsalary + $salary ));
+
+done
+echo "total salary:"
--- empwagecompution.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
- echo "welcome"
-echo "welcome"
